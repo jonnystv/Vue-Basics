a4b84e39d62050de79ede9e59afc48a3164ee9b4
jonnystv <jonathan@darefoundation.com>
2021-01-11 22:27:17 +0000

    app.js edited

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
-        el: "#app"
+        el: '#app',
+        data: {
+            todos: [
+                'Get Shopping', 'Cook Dinner', 'Wash Dishes'
+            ],
+            newTodo: ""
+        },
+        methods: {
+            saveNewTodo: function(){
+                this.todos.push(this.newTodo);
+                this.newTodo = "";
+            },
+        },
     });
 });
